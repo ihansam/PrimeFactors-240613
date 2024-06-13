6e7f4a2879524cb9c42069e02ce5102b03d22365
kidan.jin <ihansam0322@gmail.com>
2024-06-13 09:38:38 +0900

    [feature] 2

--- prime_factors.py
+++ prime_factors.py
@@ -1,4 +1,6 @@
 class PrimeFactor:
     def of(self, number: int) -> list[int]:
         factors = []
+        if number == 2:
+            factors.append(2)
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -7,3 +7,8 @@ class TestPrimeFactor(TestCase):
     def test_prime_factor_of_1(self):
         pf = PrimeFactor()
         self.assertEqual([], pf.of(1))
+
+    def test_prime_factor_of_2(self):
+        pf = PrimeFactor()
+        self.assertEqual([2], pf.of(2))
+
