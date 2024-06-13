4ec3b4f22f6ea98aef00fae66be1c1aff620db0e
kidan.jin <ihansam0322@gmail.com>
2024-06-13 09:39:49 +0900

    [refactoring] 2

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,11 +4,14 @@ from prime_factors import PrimeFactor
 
 
 class TestPrimeFactor(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.prime_factor = PrimeFactor()
+
     def test_prime_factor_of_1(self):
-        pf = PrimeFactor()
-        self.assertEqual([], pf.of(1))
+        self.assertEqual([], self.prime_factor.of(1))
 
     def test_prime_factor_of_2(self):
-        pf = PrimeFactor()
-        self.assertEqual([2], pf.of(2))
+        self.assertEqual([2], self.prime_factor.of(2))
 
