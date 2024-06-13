5866d9a9a0b427c07033dc4b68db103885b0eda5
kidan.jin <ihansam0322@gmail.com>
2024-06-13 09:54:27 +0900

    [refactoring] 7

--- prime_factors.py
+++ prime_factors.py
@@ -1,14 +1,10 @@
 class PrimeFactor:
     def of(self, number: int) -> list[int]:
         factors = []
-        if number > 1:
-            divisor = 2
-            if number in [4, 6, 9, 12]:
-                while number > 1:
-                    while number % divisor == 0:
-                        factors.append(divisor)
-                        number //= divisor
-                    divisor += 1
-            else:
-                factors.append(number)
+        divisor = 2
+        while number > 1:
+            while number % divisor == 0:
+                factors.append(divisor)
+                number //= divisor
+            divisor += 1
         return factors
